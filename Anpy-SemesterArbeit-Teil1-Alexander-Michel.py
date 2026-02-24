ede2c3a0fdfb005d6db4742463ccbf978aa81e62
alex <alexander.michel@students.ffhs.ch>
2026-02-24 21:23:11 +0100

    Add some changes (mostly comments for easier code reading and understanding)

--- Anpy-SemesterArbeit-Teil1-Alexander-Michel.py
+++ Anpy-SemesterArbeit-Teil1-Alexander-Michel.py
@@ -1,21 +1,21 @@
 # Aufgabe 1. (Implementieren Sie eine Python-Funktion fib(n), die die n-te Fibonacci-Zahl bestimmt. Für n >= 2)
 def fib(n):
+    # Prüfen, ob n grösser oder gleich 2 ist, falls ja, wird das ergebnis 1
     if n <= 2:
       f = 1
+    # Falls n nicht kleiner oder gleich als 2 ist, wird
     else:
-      f = n-1+n-2
+      f = fib(n-1)+fib(n-2)
     return f
 
-print(fib(7))
-
 # Aufgabe 2. (Eine naive Implementierung setzt die obige Rekursionsgleichung direkt um. Schreiben Sie eine weitere Python-Funktion, die berechnet, wie viele Funktionsaufrufe on fib notwendig sind, um die n-te Fibonacci-Zahl zu berechnen.)
-aufrufe = [0]
+# Globale Variable "aufrufe" für die Anzahl Funktionsaufrufe zu speichern
+aufrufe = 0
 def fib_aufrufe(n):
     global aufrufe
-    aufrufe[0] += 1
-    if n==1:
-      f = 1
-    elif n==2:
+    # Hier wird "aufrufe" um einen Wert erhöht
+    aufrufe += 1
+    if n <= 2:
       f = 1
     else:
       f = fib_aufrufe(n-1)+fib_aufrufe(n-2)
@@ -26,22 +26,26 @@ print(fib_aufrufe(7), aufrufe)
 print(fib_aufrufe(8), aufrufe)
 
 # Aufgabe 3. (Vergleichen Sie die Anzahl der Funktionsaufrufe von fib zur Bestimmung einer Fibonacci-Zahl mit den Fibonacci-Zahlen selber. Können Sie eine Vermutung aufstellen?)
+# Da wird eine Funktion auf einen fremden Bibliothek verwenden, muss diese hier importiert werden
 import time
+# te (steht für timeend) ist eine Globale Variable um die Funktionsdauer zu speichern
 te =  0
 aufrufe = 0
-aufrufe_liste = [0]
 
 def fib_aufrufe_graph(n):
     global aufrufe, te
-    tr = time.time()
+    # ts (steht für timestart) um wird benötigt um die Startzeit der Funktion zu speichern
+    ts = time.time()
     aufrufe += 1
     if n <= 2:
       f = 1
     else:
       f = fib_aufrufe_graph(n-1)+fib_aufrufe_graph(n-2)
-    te = time.time()-tr
+    # Hier wird der Endzeitpunk minus den Startzeitpunk gerechnet, um die Dauer der Funktion zu erhalten
+    te = time.time()-ts
     return f
 
+# Hier wird jeweils die Funktion ausgerufen, die Dauer und Anzahl Funktionsaufrufe werden gespeichert, damit diese graphisch dargestellt werden können.
 print(fib_aufrufe_graph(2), " 2")
 aufrufe2 = aufrufe
 Dauer2 = te
@@ -61,13 +65,13 @@ print(fib_aufrufe_graph(7))
 aufrufe7 = aufrufe
 Dauer7 = te
 
+# Libary importiern, damit die Werte graphisch dargestellt werdenk können.
 import matplotlib.pyplot as plt
 
 #Anzahl aufrufe mit Fibonaccizahl vergleichen
 xiterativewerte=[1, 2, 3, 5, 8, 13]
 yiterativewerte= [aufrufe2, aufrufe3, aufrufe4, aufrufe5, aufrufe6, aufrufe7]
 
-#plt.bar(xiterativewerte, yiterativewerte)
 plt.plot(xiterativewerte, yiterativewerte)
 plt.scatter(xiterativewerte, yiterativewerte)
 
@@ -79,11 +83,11 @@ plt.show()
 te =  0
 def fib(n):
     global te
-    tr = time.time()
+    ts = time.time()
     if n < 2:
         return n
     p = fib(n-1)+fib(n-2)
-    te = time.time()-tr
+    te = time.time()-ts
     return p
 
 #Dauer der Berechung mit berechneter Fibonaccizahl vergleichen
@@ -100,6 +104,7 @@ plt.show()
 # Aufgabe 5. (Implementieren Sie eine weitere Python-Funktion zur Berechnung der n-ten Fibonacci-Zahl, die möglichst effizient ist. (Hinweis: das kann rekursiv oder iterativ gelöst werden.))
 
 # Iterative Fibonacci-Funktion
+# Hier heisst die Variable nicht "aufrufe" sondern "count", gespeichert werden die Anzahl Funktionsaufrufe in der iterativen Funktion
 count = 0
 te = 0
 def fib_interativ(n):
@@ -131,7 +136,7 @@ print(fib_interativ(7))
 aufrufe7 = count
 Dauer7 = te
 
-#Anzahl aufrufe mit Fibonaccizahl vergleichen
+#Anzahl Aufrufe mit Fibonaccizahl vergleichen
 xiterativewerte=[1, 2, 3, 5, 8, 13]
 yiterativewerte= [aufrufe2, aufrufe3, aufrufe4, aufrufe5, aufrufe6, aufrufe7]
 
