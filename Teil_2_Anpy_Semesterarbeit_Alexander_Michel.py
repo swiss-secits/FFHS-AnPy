0546823b225d3dca57a74a6741579dba1c3b6748
alex <alexander.michel@students.ffhs.ch>
2026-03-02 23:24:57 +0100

    add new files (new termpaper part [2])

---
+++ Teil_2_Anpy_Semesterarbeit_Alexander_Michel.py
@@ -0,0 +1,109 @@
+from email.policy import default
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 5, 0.1)
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
+
+
+
+#	Funktionsgraphen
+
+
+#	Mehrere Funktionsgraphen in der selben Graphik
+#	Balkendiagramme
+# Source Code: https://matplotlib.org/stable/gallery/lines_bars_and_markers/bar_label_demo.html
+species = ('Adelie', 'Chinstrap', 'Gentoo')
+sex_counts = {
+    'Male': np.array([73, 34, 61]),
+    'Female': np.array([73, 34, 58]),
+}
+width = 0.6  # the width of the bars: can also be len(x) sequence
+
+
+fig, ax = plt.subplots()
+bottom = np.zeros(3)
+
+for sex, sex_count in sex_counts.items():
+    p = ax.bar(species, sex_count, width, label=sex, bottom=bottom)
+    bottom += sex_count
+
+    ax.bar_label(p, label_type='center')
+
+ax.set_title('Number of penguins by sex')
+ax.legend()
+
+plt.show()
+
+#	Tortendiagramme
+# Source Code: https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html
+fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+
+recipe = ["375 g flour",
+          "75 g sugar",
+          "250 g butter",
+          "300 g berries"]
+
+data = [float(x.split()[0]) for x in recipe]
+ingredients = [x.split()[-1] for x in recipe]
+
+
+def func(pct, allvals):
+    absolute = int(np.round(pct/100.*np.sum(allvals)))
+    return f"{pct:.1f}%\n({absolute:d} g)"
+
+
+wedges, texts, autotexts = ax.pie(data, autopct=lambda pct: func(pct, data),
+                                  textprops=dict(color="w"))
+
+ax.legend(wedges, ingredients,
+          title="Ingredients",
+          loc="center left",
+          bbox_to_anchor=(1, 0, 0.5, 1))
+
+plt.setp(autotexts, size=8, weight="bold")
+
+ax.set_title("Matplotlib bakery: A pie")
+
+plt.show()
+data = 0
+fig = 0
+#	Histogramme
+# Source Code: https://matplotlib.org/stable/gallery/animation/animated_histogram.html
+
+import functools
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib.animation as animation
+# Setting up a random number generator with a fixed state for reproducibility.
+rng = np.random.default_rng(seed=19680801)
+# Fixing bin edges.
+HIST_BINS = np.linspace(-4, 4, 100)
+
+# Histogram our data with numpy.
+data = rng.standard_normal(1000)
+n, _ = np.histogram(data, HIST_BINS)
+
+def animate(frame_number, bar_container):
+    data = rng.standard_normal(1000)
+    n, _ = np.histogram(data, HIST_BINS)
+    for count, rect in zip(n, bar_container.patches):
+        rect.set_height(count)
+
+    return bar_container.patches
+
+fig, ax = plt.subplots()
+_, _, bar_container = ax.hist(data, HIST_BINS, lw=1,
+                              ec="yellow", fc="green", alpha=0.5)
+ax.set_ylim(top=55)  # set safe limit to ensure that all data is visible.
+
+anim = functools.partial(animate, bar_container=bar_container)
+ani = animation.FuncAnimation(fig, anim, 50, repeat=False, blit=True)
+plt.show()
+#animation.Animation.to_html5_video(plt.show())
+
